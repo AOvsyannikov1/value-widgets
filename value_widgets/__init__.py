0a9dffe0d7f515ffec25c6ede032352f13f88fb4
Овсянников Андрей Владимирович <ovsyannikov.andrey@mtz.tex>
2025-08-26 13:37:39 +0300

    Edited hooks in __init__.py

--- value_widgets/__init__.py
+++ value_widgets/__init__.py
@@ -7,3 +7,10 @@ from .value_widget import ValueWidget
 from .error_widget import ErrorWidget
 from .pointer_device import PointerDevice
 from .timer_widget import TimerWidget
+
+
+def _get_hook_dirs():
+    """Возвращает пути к директориям с хуками PyInstaller"""
+    import os
+    return [os.path.join(os.path.dirname(__file__), 'hooks')]
+
